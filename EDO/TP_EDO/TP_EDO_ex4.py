c0cd8c35f67fdb94f1eec4064bed7c397b84ce0c
gdelay <gui.delay@laposte.net>
2022-03-17 14:52:37 +0100

    TP EDO : exo 4 -> OK

--- EDO/TP_EDO/TP_EDO_ex4.py
+++ EDO/TP_EDO/TP_EDO_ex4.py
@@ -1,17 +1,23 @@
-import numpy as np
+limport numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
+import numpy.linalg as LA
 
-#################  Exercice 3 : Etude du pendule, simulations numeriques
+#################  Exercice 4 : Etude du pendule, simulations numeriques
 ## etude du pendule
-
 ##### Parametres
 g = 9.81
 l = 5.
+X0 = np.array([np.pi/3,0])
+T = 12
+N = 200
 
 def f(X,t):
     return np.array([X[1], -g/l*np.sin(X[0])])
+def df(X,t):
+    return np.array([[0, 1],[-g/l*np.cos(X[0]), 0]])
 
+######## schemas
 ## schema d'Euler explicite
 def Euler(X0,N,T):
     dt = T/N
@@ -41,15 +47,37 @@ def milieu(X0,N,T):
 
     return X
 
-## donnees simu
-X0 = np.array([np.pi/3,0])
-T = 2
-N = 200
+## methode de Newton
+def Newton(f,df,x0,tol,maxiter):
+    x = x0
+    it = 0
+    delta_norm = 10 * tol
+    while delta_norm > tol and it < maxiter :
+        delta_x = np.dot( LA.inv( df(x) ) , f(x) )
+        x = x - delta_x
+        it += 1
+        delta_norm = LA.norm(delta_x)
+    return x
 
-## remarque : pour T = 10 et N = 200, on voit tres bien l'effet d'amplification du schema d'Euler,
-## on pourrait faire une question la-dessus
+## schema d'Euler implicite
+## df : jacobienne de f par rapport a l'espace
+def Euler_imp(f,df,X0,N,T):
+    dt = T/N # pas de temps
+    t=0 # temps
+    X = np.zeros([N+1,2])
+    X[0] = X0
+    ## boucle en temps
+    for i in range(0,N):
+        def F(z):
+            return z - X[i] - dt * f(z,t+dt)
+        def DF(z):
+            return np.identity(2) - dt * df(z,t+dt)
+        X[i+1] = Newton(F,DF,X[i],0.000001,10)
+        t += dt
+    return X
 
-# Euler
+
+# Euler explicite
 X_Euler = Euler(X0,N,T)
 theta_Euler , omega_Euler = X_Euler[:,0] , X_Euler[:,1]
 # point milieu
@@ -60,12 +88,17 @@ tps = np.linspace(0,T,N+1)
 # odeint
 sol_ODE = integrate.odeint(f,X0,tps)
 theta_ode , omega_ode = sol_ODE[:,0] , sol_ODE[:,1]
+# Euler implicite
+sol_EI = Euler_imp(f,df,X0,N,T)
+theta_EI, omega_EI = sol_EI[:,0], sol_EI[:,1]
+
 
 ## affichage de theta
 plt.figure()
-plt.plot(tps,theta_Euler,label="Euler")
+plt.plot(tps,theta_Euler,label="Euler explicite")
 plt.plot(tps,theta_milieu,label="point milieu")
 plt.plot(tps,theta_ode,label="odeint")
+plt.plot(tps,theta_EI,label="Euler implicite")
 plt.xlabel("t")
 plt.ylabel("theta")
 plt.title("Theta(t)")
@@ -74,9 +107,10 @@ plt.show()
 
 ## affichage de omega
 plt.figure()
-plt.plot(tps,omega_Euler,label="Euler")
+plt.plot(tps,omega_Euler,label="Euler explicite")
 plt.plot(tps,omega_milieu,label="point milieu")
 plt.plot(tps,omega_ode,label="odeint")
+plt.plot(tps,omega_EI,label="Euler implicite")
 plt.xlabel("t")
 plt.ylabel("omega")
 plt.title("Omega(t)")
@@ -85,9 +119,10 @@ plt.show()
 
 ## portrait de phases
 plt.figure()
-plt.plot(theta_Euler,omega_Euler,label="Euler")
+plt.plot(theta_Euler,omega_Euler,label="Euler explicite")
 plt.plot(theta_milieu,omega_milieu,label="point milieu")
 plt.plot(theta_ode,omega_ode,label="odeint")
+plt.plot(theta_EI,omega_EI,label="Euler implicite")
 plt.xlabel("theta")
 plt.ylabel("omega")
 plt.title("Comportement des differents schemas dans l'espace des phases")
